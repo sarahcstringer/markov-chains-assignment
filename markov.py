a21e13c44d1d22571936789ac54cb98698193643
Hackbright Student <info@hackbrightacademy.com>
2016-04-13 11:53:38 -0700

    Creates n-grams based on user input

--- markov.py
+++ markov.py
@@ -1,5 +1,5 @@
 from random import choice
-
+import sys
 
 def open_and_read_file(file_path):
     """Takes file path as string; returns text as string.
@@ -15,7 +15,7 @@ def open_and_read_file(file_path):
     # return "This should be a variable that contains your file text as one long string"
 
 
-def make_chains(text_string):
+def make_chains(text_string, n=2):
     """Takes input text as string; returns _dictionary_ of markov chains.
 
     A chain will be a key that consists of a tuple of (word1, word2)
@@ -32,9 +32,19 @@ def make_chains(text_string):
     
     chains = {}
 
-    for i in range(len(text) - 2):
-        word_pair = (text[i], text[i + 1])
-        chains[word_pair] = chains.get(word_pair, []) + [text[i + 2]]
+# n-grams
+    for i in range(len(text) - n):
+        words = []
+        for j in range(n):
+            words.append(text[i + j])
+        words = tuple(words)
+        chains[words] = chains.get(words, []) + [text[i + n]]
+
+
+# bi-grams:
+    # for i in range(len(text) - 2):
+    #     word_pair = (text[i], text[i + 1])
+    #     chains[word_pair] = chains.get(word_pair, []) + [text[i + 2]]
 
 
         # word_pair = (text[i], text[i + 1])
@@ -52,7 +62,7 @@ def make_chains(text_string):
         #     print ("adding value", new_value, "for key", word_tuple)            
         #     chains[word_tuple] = new_value
             
-
+    print chains
     return chains
 
 
@@ -62,7 +72,8 @@ def make_text(chains):
     
     current_key = choice(chains.keys())
     text = " ".join(current_key)
-    
+
+
     while True:
         if current_key in chains:
             string = choice(chains[current_key])
@@ -71,6 +82,15 @@ def make_text(chains):
 
         else:
             return text
+
+    # while len(text) < 20:
+
+    #     string = choice(chains[current_key])
+    #     text = "{} {}".format(text, string)
+    #     current_key = (current_key[1], string)
+
+    # return text
+
 
 
         # if current_key not in chains:
@@ -81,13 +101,15 @@ def make_text(chains):
         # current_key = (current_key[1], string)
       
 
-input_path = "gettysburg.txt"
+# input_path = "hello.txt"
+input_path = sys.argv[1]
+n = int(sys.argv[2])
 
 # Open the file and turn it into one long string
 input_text = open_and_read_file(input_path)
 
 # Get a Markov chain
-chains = make_chains(input_text)
+chains = make_chains(input_text, n)
 
 # Produce random text
 random_text = make_text(chains)
